0e88a28ff01f6088947c769e96d2cdd95597ba36
Osamudiamen <osas.azamegbe@gmail.com>
2022-08-13 19:47:10 +0100

    fix: format

--- playlistmover/playlistmover/views.py
+++ playlistmover/playlistmover/views.py
@@ -4,9 +4,9 @@ from rest_framework.views import APIView
 
 class PlaylistApiView(APIView):
     """
-    API View to manage retrieving and creating playlists from third-party music platforms for the caller.
-    Playlist retrieval done through the GET HTTP method.
-    Playlist creation done through the POST HTTP method.
+    API View to manage retrieving and creating playlists from 
+    third-party music platforms for the caller.
+    
     Authentication and Permissions are not required at this time.
     """
 
@@ -15,7 +15,7 @@ class PlaylistApiView(APIView):
         Returns List of playlists from account and platform specified in the request.
         """
         return Response({"playlists": []})
-    
+
     def post(self, request, format=None):
         """
         Creates List of playlists on account and platform specified in the request.
