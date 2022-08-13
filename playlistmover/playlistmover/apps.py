520c9df71a28fa90247937144f613e5ff96d7557
Osamudiamen <osas.azamegbe@gmail.com>
2022-08-13 16:17:10 +0100

    black formatter

--- playlistmover/playlistmover/apps.py
+++ playlistmover/playlistmover/apps.py
@@ -2,5 +2,5 @@ from django.apps import AppConfig
 
 
 class PlaylistmoverConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'playlistmover'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "playlistmover"
