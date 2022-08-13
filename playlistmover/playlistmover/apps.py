2b78eecc45d3c64de4f128b8b43ab41d659dcc2b
Osamudiamen <osas.azamegbe@gmail.com>
2022-08-13 17:00:38 +0100

    pylintrc

--- playlistmover/playlistmover/apps.py
+++ playlistmover/playlistmover/apps.py
@@ -2,5 +2,7 @@ from django.apps import AppConfig
 
 
 class PlaylistmoverConfig(AppConfig):
+    """Configuration for playlistmover app"""
+
     default_auto_field = "django.db.models.BigAutoField"
     name = "playlistmover"
