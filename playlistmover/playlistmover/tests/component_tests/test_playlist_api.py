9144ca9f06da29702b7128097e03204434aa465a
Osamudiamen <osas.azamegbe@gmail.com>
2022-08-20 13:13:14 +0100

    FEAT:move to pytest

--- playlistmover/playlistmover/tests/component_tests/test_playlist_api.py
+++ playlistmover/playlistmover/tests/component_tests/test_playlist_api.py
@@ -1,20 +1,18 @@
+"""
+    Test module for Playlist API
+"""
+import pytest
 from django.urls import reverse
 from rest_framework import status
-from rest_framework.test import APITestCase
 
 
-class TestPlaylistApiView(APITestCase):
-    """
-    Test module for Playlist API
-    """
+def test_get_playlist_without_platform(api_client):
+    """Test error response when platform query parameter is missing"""
+    url = reverse("playlists")
+    query_params = {"code": "dummycode", "state": "dummystate"}
+    expected_response = {"success": False, "error": "`platform` in request is invalid."}
 
-    def test_get_playlist_without_platform(self):
-        """Test error response when platform query parameter is missing"""
-        url = reverse("playlists")
-        query_params = {"code": "dummycode", "state": "dummystate"}
-        expected_response = {"success": False, "error": "`platform` in request is invalid."}
+    response = api_client.get(url, data=query_params, format="json")
 
-        response = self.client.get(url, data=query_params, format="json")
-
-        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
-        self.assertDictEqual(response.json(), expected_response)
+    assert response.status_code == status.HTTP_400_BAD_REQUEST
+    assert response.json() == expected_response
